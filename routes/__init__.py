a923c0afeea4c0ee2906ab5565486fb735d442d2
Noah Huppert <contact@noahh.io>
2018-11-11 00:19:15 -0500

    added config field to routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,12 @@
 import falcon
 
 from .health import HealthCheckResource
+from config import Config
 
-def add_routes(api: falcon.API):
+def add_routes(api: falcon.API, cfg: Config):
     """ Registers endpoints with Falcon
     Args:
         - api: Falcon API object
+        - cfg: Configuration
     """
     api.add_route(HealthCheckResource.Path, HealthCheckResource())
